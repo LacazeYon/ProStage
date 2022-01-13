2e4fdf664d07f6e5f4db4e4ef36fdb52fa8d5ef6
Lacaze Yon <lacaze.yon@iutbayonne.univ-pau.fr>
2022-01-13 14:49:58 +0100

    fin d'implémentation de toute les fonctionnalité

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -5,6 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Formation;
+use App\Entity\Entreprise;
+use App\Entity\Stage;
+use App\Repository\RepositoryFormation;
+use App\Repository\RepositoryEntreprise;
+use App\Repository\RepositoryStage;
 
 class ProStageController extends AbstractController
 {
@@ -13,8 +19,12 @@ class ProStageController extends AbstractController
      */
     public function index(): Response
     {
+        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+        $stages = $repositoryStage->findAll();
+
         return $this->render('pro_stage/index.html.twig', [
             'controller_name' => 'ProStageController',
+            'stages' => $stages,
         ]);
     }
     /**
@@ -23,7 +33,7 @@ class ProStageController extends AbstractController
     public function index_entreprises(): Response
     {
         $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
-        $entreprises = $repositoryEntreprise->find();
+        $entreprises = $repositoryEntreprise->findAll();
         return $this->render('pro_stage/affichage_entreprise.html.twig', [
             'controller_name' => 'ProStageController',
             'entreprises' => $entreprises,
@@ -34,8 +44,11 @@ class ProStageController extends AbstractController
      */
     public function index_formations(): Response
     {
+        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
+        $formations = $repositoryFormation->findAll();
         return $this->render('pro_stage/affichage_formation.html.twig', [
             'controller_name' => 'ProStageController',
+            'formations' => $formations,
         ]);
     }
     /**
@@ -43,9 +56,12 @@ class ProStageController extends AbstractController
      */
     public function index_stage($id): Response
     {
+        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+        $stage = $repositoryStage->find($id);
+
         return $this->render('pro_stage/affichage_stage_id.html.twig', [
             'controller_name' => 'ProStageController',
-            'id' => $id,
+            'stage' => $stage,
         ]);
     }
     /**
@@ -55,6 +71,38 @@ class ProStageController extends AbstractController
     {
         return $this->render('pro_stage/filtrer.html.twig', [
             'controller_name' => 'ProStageController',
+        ]);
+    }
+    /**
+     * @Route("/stage_trierParFormation/{idFormation}", name="pro_stage_stage_par_formation")
+     */
+    public function index_stage_par_formation($idFormation): Response
+    {
+
+        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
+        $formation = $repositoryFormation->find($idFormation);
+        
+
+        return $this->render('pro_stage/affichage_stage_par_formation.html.twig', [
+            'controller_name' => 'ProStageController',
+            'formation' => $formation,
+        ]);
+    }
+    /**
+     * @Route("/stage_trierParEntreprise/{idEntreprise}", name="pro_stage_stage_par_entreprise")
+     */
+    public function index_stage_par_entreprise($idEntreprise): Response
+    {
+        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+        $stages = $repositoryStage->findBy(['entreprise' => $idEntreprise]);
+        
+        $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
+        $entreprise = $repositoryEntreprise->find($idEntreprise);
+
+        return $this->render('pro_stage/affichage_stage_par_entreprise.html.twig', [
+            'controller_name' => 'ProStageController',
+            'stages' => $stages,
+            'entreprise' => $entreprise,
         ]);
     }
 }
